e7e1bb1f827aa8a079feb2cf934771bfecba192b
fareehaabbasi <abbasifareeha9@gmail.com>
2025-12-19 01:56:57 +0500

    home main part complete

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const HomeBottomText = () => {
   return (
     <div className='font-[font500] flex items-end justify-center gap-5 mt-14'>
-      <div className='border-3 h-22 flex items-center px-6 border-white rounded-full uppercase'>
+      <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-22 flex items-center px-6 border-white rounded-full uppercase'>
         <Link className='text-[7vw] mt-6' to='/projects'>Work</Link>
       </div>
-      <div className='border-3 h-22 flex items-center px-6 border-white rounded-full uppercase'>
+      <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-22 flex items-center px-6 border-white rounded-full uppercase'>
         <Link className='text-[7vw] mt-6' to='agency'>Agency</Link>
       </div>
     </div>
