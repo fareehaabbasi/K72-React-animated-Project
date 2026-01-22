cc7afde21719ef6fc4520ded222d7df2f1aefc7f
fareehaabbasi <abbasifareeha9@gmail.com>
2026-01-22 17:50:02 +0500

    still-working

---
+++ src/components/Navigation/FullScreenNav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <h1>Work</h1>
+            <div>
+                <h2>See Everything</h2>
+                <img src="/assets/images/NavbarScreen/1st-line.jpg" alt="" />
+                <h2>See Everything</h2>
+                <img src="/assets/images/NavbarScreen/1st-line.jpg" alt="" />
+            </div>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
